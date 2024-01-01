4879283cd8322ab665c054424e2aeb845fb93801
Davi de Oliveira <odavi278@gmail.com>
2023-12-31 22:56:41 -0300

    Finalizando cronometro

--- Stopwatch/Program.cs
+++ Stopwatch/Program.cs
@@ -19,7 +19,27 @@ namespace Stopwatch
             Console.WriteLine("Quanto tempo que contar? ");
 
             string data = Console.ReadLine().ToLower();
-            Console.WriteLine(data);
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+            int multiplier = 1;
+            if (type == 'm')
+                multiplier = 60;
+            if (time == 0)
+                System.Environment.Exit(0);
+            PreStart(time * multiplier);
+        }
+
+        static void PreStart(int time)
+        {
+            Console.Clear();
+            Console.WriteLine("Ready...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Set...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Go!!");
+            Thread.Sleep(2500);
+
+            Start(time);
         }
         static void Start(int time)
         {
@@ -35,6 +55,7 @@ namespace Stopwatch
             Console.Clear();
             Console.WriteLine("Stopwatch finalizado... ");
             Thread.Sleep(2500);
+            Menu();
         }
     }
 }
