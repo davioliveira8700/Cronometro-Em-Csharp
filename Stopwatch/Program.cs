819070773ddd5955b59b413e5350647274297bb6
Davi de Oliveira <odavi278@gmail.com>
2023-12-31 22:07:23 -0300

    Criando a estrutura do cronometro

--- Stopwatch/Program.cs
+++ Stopwatch/Program.cs
@@ -8,5 +8,15 @@ namespace Stopwatch
         {
 
         }
+        static void Start()
+        {
+            int time = 10;
+            int currentTime = 0;
+
+            while(currentTime != time)
+            {
+                currentTime++;
+            }
+        }
     }
 }
